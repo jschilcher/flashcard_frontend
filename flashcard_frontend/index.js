0fe9d71283b725eb8ad45bebb0480fdace087dbe
Jenelle Schilcher <jschilcher8@gmail.com>
2021-07-05 18:00:32 -0500

    Add app.jsx

---
+++ flashcard_frontend/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const jsxElement = <h1>App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(jsxElement, document.getElementById("root"));
